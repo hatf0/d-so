47b7a9e16067645c5c8963e73760cd9d97d489da
ohninedotseven <harrison@malw.io>
2018-02-02 11:20:46 -0700

    noot noot

--- decompile.d
+++ decompile.d
@@ -101,19 +101,33 @@ enum CallTypes {
 
 File curFile;
 
-void decompile(char[] global_st, char[] function_st, double[] global_ft, double[] function_ft, int[] code, int[] lbptable, string dso_name) {
+void decompile(char[] global_st, char[] function_st, double[] global_ft, double[] function_ft, int[] code, int[] lbptable, string dso_name = "", bool entered_function = false, int offset = 0, int tablevel = 0) {
 	import std.algorithm, std.string;
 	writeln("Code length: ", code.length);
 	int i = 0;
 	bool create_folders = false;
-	int indentation_level = 0;
-	bool enteredFunction = false;
+	int indentation_level = tablevel;
+	bool enteredFunction = entered_function, enteredObjectCreation = false; //Needed if we enter into an object creation for some reason?
 	string[] string_stack;
-	int[] int_stack;
-	double[] float_stack;
+	string[] int_stack;
+	string[] float_stack;
+	string[] bin_stack;
 	string[][] arguments;
 	int[] lookback_stack = [0, 0, 0, 0];
 	string current_object = "", current_field = "", current_variable = "";
+	string string_op(char inchar) {
+		switch(inchar) {
+			case '\n':
+				return "NL";
+			case '\t':
+				return "TAB";
+			case ' ':
+				return "@";
+
+			default:
+				return "";
+		}
+	}
 	string constructPrettyFunction(string fnName, string fnNamespace, string[] argv, CallTypes callType = CallTypes.FunctionCall) {
 		string retVal = "";
 		if(fnNamespace != "") {
@@ -164,6 +178,17 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 		return text(slicedString.ptr);
 	}
 
+	float get_float(int offset, bool fuck = enteredFunction) {
+		float retval;
+		if(!fuck) {
+			retval = global_ft[offset];
+		}
+		else {
+			retval = function_ft[offset];
+		}
+		return retval;
+	}
+
 	string addTabulation(string previous) {
 		string retVal = "";
 		for(int l = 0; l < indentation_level; l++) {
@@ -173,10 +198,12 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 		return retVal;
 	}
 
-	int fileExtension = cast(int)countUntil(dso_name, ".cs.dso");
-	string file_name_with_fixed_ext = dso_name[0..fileExtension] ~ ".cs";
-	//writeln(dso_name[0..fileExtension]);
-	curFile = File(file_name_with_fixed_ext, "w");
+	if(dso_name != "") { //If this happens, then, we're probably doing a partial decompile.
+		int fileExtension = cast(int)countUntil(dso_name, ".cs.dso");
+		string file_name_with_fixed_ext = dso_name[0..fileExtension] ~ ".cs";
+		//writeln(dso_name[0..fileExtension]);{
+		curFile = File(file_name_with_fixed_ext, "w");
+	}
 	while(i < code.length) {
 		opcodes opcode = cast(opcodes)code[i];
 		i++;
@@ -221,11 +248,12 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 						argv ~= get_string(code[i + 6 + q]);
 					//	argv ~= text(function_st[code[i + 6 + q]]);
 					}
-					curFile.write("function " ~ constructPrettyFunction(fnName, fnNamespace, argv) ~ " {\n");
+					curFile.writeln("function " ~ constructPrettyFunction(fnName, fnNamespace, argv) ~ " {");
 					indentation_level++;
 					//writeln(constructPrettyFunction(fnName, fnNamespace, argv));
-					i += 6 + argc;
-					//i = fnEndLoc - 1;
+					//i += 6 + argc;
+					decompile(global_st, function_st, global_ft, function_ft, code[i + 6 + argc..fnEndLoc - 2], lbptable, "", enteredFunction, i + 6 + argc, indentation_level);
+					i = fnEndLoc - 1;
 					writeln(argv);
 					break;
 				}
@@ -234,10 +262,7 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					writeln("encountered endfunc at ", i - 1);
 					code = code.remove(i - 1); //we encountered it, now delete it because offsets are fucky
 					indentation_level--; //tabs or spaces??
-					for(int b = 0; b < indentation_level; b++) {
-						curFile.write("\t");
-					}
-					curFile.write("}\n");
+					curFile.writeln(addTabulation("}"));
 					writeln("code at pos: ", code[i - 1]);
 					enteredFunction = false;
 					i--;
@@ -254,7 +279,10 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					if(code[i + 1]) {
 						fnNamespace = get_string(code[i + 1], false);
 					}
+					writeln(arguments);
 					string[] argv = arguments[arguments.length - 1];
+					arguments = arguments.remove(arguments.length - 1);
+					//writeln(argv);
 					string_stack ~= constructPrettyFunction(fnName, fnNamespace, argv, cast(CallTypes)call_type);
 					i += 3;
 					break;
@@ -274,8 +302,20 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					}
 
 					if(i != code.length && code[i] != opcodes.DECOMPILER_ENDFUNC) {
-						curFile.write(writeOut ~ "\n");
+						curFile.writeln(writeOut);
 					}
+					break;
+				}
+
+				case opcodes.OP_PUSH_FRAME: {
+					arguments ~= [[]];
+					writeln(arguments);
+					break;
+				}
+
+				case opcodes.OP_PUSH: {
+					//if(lookback_stack[3] == opcodes.OP_LOADVAR_FLT || lookback_stack[3] == opcodes.OP_LOADVAR_STR || lookback_stack[3] == opcodes.OP_LOADVAR_FLT) {
+					arguments[arguments.length - 1] ~= [popOffStack(string_stack)];
 					break;
 				}
 
@@ -283,6 +323,11 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					string parent = get_string(code[i], false);
 					int isDatablock = code[i + 1], failJump = code[i + 2];
 					string constr = "new";
+					break;
+				}
+
+				case opcodes.OP_ADD_OBJECT: {
+					i++;
 					break;
 				}
 
@@ -296,8 +341,14 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					break;
 				}
 
-				case opcodes.OP_ADD_OBJECT: {
+				case opcodes.OP_SETCURVAR, opcodes.OP_SETCURVAR_CREATE: {
+					current_variable = get_string(code[i], false);
 					i++;
+					break;
+				}
+
+				case opcodes.OP_SETCURVAR_ARRAY, opcodes.OP_SETCURVAR_ARRAY_CREATE: {
+					current_variable = popOffStack(string_stack);
 					break;
 				}
 
@@ -312,13 +363,21 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					break;
 				}
 
-				case opcodes.OP_LOADVAR_STR: {
-					string_stack ~= current_variable;
+				case opcodes.OP_LOADVAR_STR, opcodes.OP_LOADVAR_FLT, opcodes.OP_LOADVAR_UINT: {
+					if(opcode == opcodes.OP_LOADVAR_STR) {
+						string_stack ~= current_variable;
+					}
+					else if(opcode == opcodes.OP_LOADVAR_FLT) {
+						float_stack ~= current_variable;
+					}
+					else if(opcode == opcodes.OP_LOADVAR_UINT) {
+						int_stack ~= current_variable;
+					}
 					break;
 				}
 
 				case opcodes.OP_LOADIMMED_STR, opcodes.OP_TAG_TO_STR: {
-					auto str = get_string(code[i]);
+					auto str = "\"" ~ get_string(code[i]) ~ "\"";
 					i++;
 					if(opcode == opcodes.OP_TAG_TO_STR) {
 						str = str.replace("'", "");
@@ -327,33 +386,162 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 					break;
 				}
 
-				case opcodes.OP_STR_TO_NONE: {
+				case opcodes.OP_LOADIMMED_IDENT, opcodes.OP_LOADIMMED_FLT, opcodes.OP_LOADIMMED_UINT: {
+					if(opcode == opcodes.OP_LOADIMMED_IDENT) {
+						string_stack ~= get_string(code[i], false);
+					}
+					else if(opcode == opcodes.OP_LOADIMMED_FLT) {
+						float_stack ~= to!string(get_float(code[i]));
+					}
+					else if(opcode == opcodes.OP_LOADIMMED_UINT) {
+						int_stack ~= to!string(code[i]);
+					}
+					i++;
+					break;
+				}
+
+				case opcodes.OP_STR_TO_NONE, opcodes.OP_FLT_TO_NONE, opcodes.OP_UINT_TO_NONE: {
 					//writeln(string_stack);
 					//Return value is ignored, so we can immediately write it out.
-					writeln(string_stack, " ", string_stack.length);
-					auto theFunc = addTabulation(popOffStack(string_stack));
+					//writeln(string_stack, " ", string_stack.length);
+					string theFunc;
+					if(opcode == opcodes.OP_STR_TO_NONE) {
+						theFunc = addTabulation(popOffStack(string_stack));
+					}
+					else if(opcode == opcodes.OP_FLT_TO_NONE) {
+						popOffStack(float_stack);
+						break;
+						//theFunc = addTabulation(popOffStack(float_stack));
+					}
+					else if(opcode == opcodes.OP_UINT_TO_NONE) {
+						popOffStack(int_stack);
+						break;
+						//theFunc = addTabulation(popOffStack(int_stack));
+					}
+					//writeln(theFunc);
 					if(theFunc[theFunc.length - 1] != ";"[0]) {
 						theFunc ~= ";";
 					}
-					curFile.write(theFunc ~ "\n");
+					curFile.writeln(theFunc);
 					break;
 				}
 
-				case opcodes.OP_PUSH_FRAME: {
-					arguments ~= [[]];
-					writeln(arguments);
+				case opcodes.OP_STR_TO_FLT, opcodes.OP_STR_TO_UINT: {
+					if(opcode == opcodes.OP_STR_TO_FLT) {
+						float_stack ~= popOffStack(string_stack);
+					}
+					else if(opcode == opcodes.OP_STR_TO_UINT) {
+						int_stack ~= popOffStack(int_stack);
+					}
 					break;
 				}
 
-				case opcodes.OP_PUSH: {
-					if(lookback_stack[3] == opcodes.OP_LOADVAR_FLT || lookback_stack[3] == opcodes.OP_LOADVAR_STR || lookback_stack[3] == opcodes.OP_LOADVAR_FLT) {
-						//Push the last loaded variable to the arguments.
-						arguments[arguments.length - 1] ~= popOffStack(string_stack);
+				case opcodes.OP_FLT_TO_STR, opcodes.OP_FLT_TO_UINT: {
+					if(opcode == opcodes.OP_FLT_TO_STR) {
+						string_stack ~= popOffStack(float_stack);
+					}
+					else if(opcode == opcodes.OP_FLT_TO_UINT) {
+						int_stack ~= popOffStack(float_stack);
+					}
+					break;
+				}
+
+				case opcodes.OP_UINT_TO_STR: {
+					string_stack ~= popOffStack(int_stack);
+					break;
+				}
+
+				case opcodes.OP_SAVEVAR_UINT, opcodes.OP_SAVEVAR_FLT, opcodes.OP_SAVEVAR_STR: {
+					string part2;
+					if(opcode == opcodes.OP_SAVEVAR_UINT) {
+						part2 = int_stack[int_stack.length - 1];
+					}
+					else if(opcode == opcodes.OP_SAVEVAR_FLT) {
+						part2 = float_stack[float_stack.length - 1];
+					}
+					else if(opcode == opcodes.OP_SAVEVAR_STR) {
+						part2 = string_stack[string_stack.length - 1];
+					}
+					if(part2[part2.length - 1] != ";"[0]) { //lmfao
+						part2 ~= ";";
+					}
+					curFile.writeln(addTabulation(current_variable ~ " = " ~ part2));
+					break;
+				}
+
+				case opcodes.OP_JMPIFNOT, opcodes.OP_JMPIF, opcodes.OP_JMPIF_NP: {
+					i++;
+					break;
+				}
+
+				case opcodes.OP_SAVEFIELD_STR, opcodes.OP_SAVEFIELD_FLT: {
+					string thing;
+					if(opcode == opcodes.OP_SAVEFIELD_STR) {
+						writeln("Breathing you in when I want you out.");
+						writeln(string_stack.length);
+						thing = string_stack[string_stack.length - 1];
 					}
 					else {
-						//Else, add a blank frmae to the arguments.
-						arguments ~= [[]];
+						thing = float_stack[float_stack.length - 1];
 					}
+					if(current_object != "") {
+						//writeln("Test");
+						if(current_object[0] == '$' || current_object[0] == '%') {
+							curFile.writeln(addTabulation(current_object ~ "." ~ current_field ~ " = " ~ thing ~ ";"));
+						}
+						else {
+							curFile.writeln(addTabulation("\"" ~ current_object ~ "\"" ~ "." ~ current_field ~ " = " ~ thing ~ ";"));
+						}
+					}
+					else {
+						//Then we're in an object creation.
+						//writeln("Tired of home");
+						if(enteredObjectCreation) {
+							int_stack ~= popOffStack(int_stack) ~ current_field ~ " = " ~ thing ~ ";";
+						}
+						else {
+							curFile.writeln(addTabulation(current_field ~ " = " ~ thing ~ ";"));
+						}
+					}
+					break;
+				}
+
+				case opcodes.OP_COMPARE_STR: {
+					string after = popOffStack(string_stack); //???
+					int_stack ~= popOffStack(string_stack) ~ " $= " ~ after;
+					break;
+				}
+
+				case opcodes.OP_REWIND_STR: {
+					if(code[i] == opcodes.OP_SETCURVAR_ARRAY || code[i] == opcodes.OP_SETCURVAR_ARRAY_CREATE) {
+						string after = popOffStack(string_stack);
+						string_stack ~= popOffStack(string_stack) ~ "[" ~ after ~ "]";
+					}
+					else {
+						string part2 = popOffStack(string_stack), part1 = popOffStack(string_stack);
+						if(string_op(part1[part1.length - 1]) != "") {
+							string_stack ~= part1[0..part1.length - 2] ~ " " ~ string_op(part1[part1.length - 1]) ~ " " ~ part2;
+						}
+						else if(part1[part1.length - 1] == ',') {
+							string_stack ~= part1 ~ part2;
+						}
+						else {
+							string_stack ~= part1 ~ " @ " ~ part2;
+						}
+					}
+					//writeln(string_stack);
+					break;
+
+				}
+
+				case opcodes.OP_ADVANCE_STR_COMMA: {
+					string_stack ~= popOffStack(string_stack) ~ ",";
+					break;
+				}
+
+				case opcodes.OP_ADVANCE_STR_APPENDCHAR: {
+					string_stack ~= popOffStack(string_stack) ~ cast(char)code[i];
+					i++;
 					break;
 				}
 
@@ -364,7 +552,7 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 			}
 		}
 		catch(RangeError) {
-			writeln("Encountered a very bad error.. at ip: ", i - 1);
+			writeln("Encountered a RangeError.. at ip: ", i - 1);
 			writeln("Opcode here is: ", code[i - 1]);
 			//writeln(code[i + 2]);
 			curFile.close();
@@ -373,10 +561,12 @@ void decompile(char[] global_st, char[] function_st, double[] global_ft, double[
 		}
 	}
 
-	writeln("you won't feel a thing");
-	writeln(arguments);
-	//writeln(lookback_stack);
-	//writeln(string_stack);
-	curFile.close();
+	if(dso_name != "") {
+		writeln("you won't feel a thing");
+		writeln(arguments);
+		//writeln(lookback_stack);
+		//writeln(string_stack);
+		curFile.close();
+	}
 	//writeln("todo");
 }
